d1023caee66ba92e2ed0d6d7a412d6e703676e44
arimura <33759663+arimura@users.noreply.github.com>
2023-10-04 15:34:23 +0900

    init xcode proj

---
+++ IMASdkDemo/IMASdkDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMASdkDemo
+//
+//  Created by Kotaro Arimura on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
